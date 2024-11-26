66955f2a9edeadcab8983fb1783bc92db31b1a22
Felix <00001094703527sp@al.educacao.sp.gov.br>
2024-11-26 10:54:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Série - Como defender um assasino',
+    'Quem sabia da verdade sobre a morte de Wes desde o início da investigação?',
+    'Jorge Castillo e Dominic sabiam do assassinato de Wes desde o início,'
+)
+
+criaCartao(
+    'Série - The 100',
+    'Em qual episódio Lexa morre e qual foi a circunstância por trás de sua morte?',
+    'Lexa morre no episódio 07 da 3ª temporada '
+)
+
+criaCartao(
+    'Série - Greys Anatomy',
+    'Qual foi o impacto do acidente de avião na 8ª temporada?',
+    'Mark e Lexie faleceram, Arizona teve a perna amputada, Cristina trauma psicológico.'
+)
+
+criaCartao(
+    'Série - Ponto Cego',
+    'Qual é o nome do irmão de Jane Doe em Blindspot e qual é seu papel na trama?',
+    'O nome do irmão de Jane é Roman,'
+)
